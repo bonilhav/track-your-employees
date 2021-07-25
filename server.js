4f87add8b14223fd5f6ebf1a2488a9019dc47f31
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-07-25 10:44:54 -0600

    added view all employees

--- server.js
+++ server.js
@@ -14,3 +14,49 @@ connection.connect((err) => {
     initPrompt();
 });
 
+const initPrompt = () => {
+    const userChoice = inquirer.prompt([{
+        name: 'userOption',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'View All Employees By Department',
+            'Add Employee',
+            'Update Employee Role',
+            'View Budget By Department',
+        ],
+    }])
+        .then(function ({ userOption }) {
+            if (userOption == 'View All Employees') {
+                viewAllEmp();
+            };
+            if (userOption === 'View All Employees By Department') {
+                viewByDept();
+            };
+            if (userOption === 'Add Employee') {
+                addEmployee();
+            };
+            if (userOption === 'Update Employee Role') {
+                updateRole();
+            };
+            if (userOption === 'View Budget By Department') {
+                budgetByDepartment();
+            };
+        })
+};
+
+const viewAllEmp = () => {
+    const allQuery = `SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS  'Last Name', 
+                    role.title AS 'Title', department.department_name AS 'Department', role.salary AS 'Salary'
+    FROM employee, role, department
+    WHERE department.id = role.department_id
+    AND role.id = employee.role_id
+    ORDER BY employee.id`
+    connection.query(allQuery, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        initPrompt();
+    });
+};
+
