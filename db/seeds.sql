3420493ca2fe4ef89687e1afa2218648de35f3a7
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-07-25 11:45:50 -0600

    changes to server and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,23 @@
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Rebecca', 'Smith', 1, null);
+VALUES ('Rebecca', 'Smith', 1, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jim', 'King', 2, 1);
+VALUES ('Ana', 'Gonzalez', 2, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Vern', 'Scott', 2, 1);
+VALUES ('Jim', 'King', 3, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Beth', 'Lively', 4, 1);
+VALUES ('Joseph', 'King', 5, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Joe', 'King', 3, 1);
+VALUES ('Marie', 'Long', 5, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Kisha', 'Alire', 5, 2);
+VALUES ('Kisha', 'Alire', 6, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('June', 'Carter', 6, null);
+VALUES ('Nick', 'Hernon', 6, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Vern', 'Scott', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Fonz', 'Anderson', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('June', 'Carter', 7, null);
 
 INSERT INTO department (department_name)
 VALUES ('Management');
@@ -20,19 +26,21 @@ VALUES ('Fraud');
 INSERT INTO department (department_name)
 VALUES ('Customer Service');
 INSERT INTO department (department_name)
-VALUES ('Data Entry');
-INSERT INTO department (department_name)
 VALUES ('Human Resources');
+INSERT INTO department (department_name)
+VALUES ('CEO');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('People Manager', 120000, 1);
 INSERT INTO role (title, salary, department_id)
+VALUES ('Human Resources Manager', 120000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Fraud Manager', 120000, 1);
+INSERT INTO role (title, salary, department_id)
 VALUES ('Fraud Analyst', 80000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Representative', 90000, 4);
+VALUES ('Representative', 90000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Data Analyst', 40000, 3);
+VALUES ('Human Resource Officer', 70000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUES ('Human Resource Officer', 75000, 5);
-INSERT INTO role (title, salary, department_id)
-VALUES ('CEO', 250000, null);
+VALUES ('CEO', 250000, 5);
--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const initPrompt = () => {
         choices: [
             'View All Employees',
             'View All Employees By Department',
+            'View All Employees By Manager',
             'Add Employee',
+            'Remove Employee',
             'Update Employee Role',
             'View Budget By Department',
         ],
@@ -34,8 +36,14 @@ const initPrompt = () => {
             if (userOption === 'View All Employees By Department') {
                 viewByDept();
             };
+            if (userOption === 'View All Employees By Manager') {
+                viewByMan();
+            };
             if (userOption === 'Add Employee') {
                 addEmployee();
+            };
+            if (userOption === 'Remove Employee') {
+                removeEmployee();
             };
             if (userOption === 'Update Employee Role') {
                 updateRole();
@@ -47,30 +55,35 @@ const initPrompt = () => {
 };
 
 const viewAllEmp = () => {
-    const allQuery = `SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS  'Last Name', 
-                    role.title AS 'Title', department.department_name AS 'Department', role.salary AS 'Salary'
+    const allQuery = `SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS Employee, 
+    role.title AS 'Title', department.department_name AS 'Department', role.salary AS 'Salary'
     FROM employee, role, department
     WHERE department.id = role.department_id
     AND role.id = employee.role_id
     ORDER BY employee.id`
     connection.query(allQuery, (err, results) => {
         if (err) throw err;
+        console.log('\n');
+            console.log('VIEW ALL EMPLOYEES');
+            console.log('\n');
         console.table(results);
         initPrompt();
     });
 };
 
 const viewByDept = () => {
-
     try {
         const deptQuery = `SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS  
         'Last Name', role.title AS 'Title', department.department_name AS 'Department' 
-    FROM employee 
-    LEFT JOIN role ON employee.role_id = role.id  
-    LEFT JOIN department ON role.department_id = department.id
-    ORDER BY department.department_name`
+        FROM employee 
+        LEFT JOIN role ON employee.role_id = role.id  
+        LEFT JOIN department ON role.department_id = department.id
+        ORDER BY department.department_name`
         connection.query(deptQuery, (err, results) => {
             if (err) throw err;
+            console.log('\n');
+            console.log('VIEW EMPLOYEE BY DEPARTMENT');
+            console.log('\n');
             console.table(results)
             initPrompt();
         });
@@ -78,7 +91,28 @@ const viewByDept = () => {
         console.log(error);
         initPrompt();
     }
+};
 
+const viewByMan = () => {
+    try {
+        const manQuery = `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.department_name AS department, employee.id,  CONCAT(employee.first_name, ' ', employee.last_name) AS employee, role.title
+        FROM employee
+        LEFT JOIN employee manager on manager.id = employee.manager_id
+        INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+        INNER JOIN department ON (department.id = role.department_id)
+        ORDER BY manager`
+        connection.query(manQuery, (err, results) => {
+            if (err) throw err;
+            console.log('\n');
+            console.log('VIEW EMPLOYEE BY MANAGER');
+            console.log('\n');
+            console.table(results)
+            initPrompt();
+        });
+    } catch (error) {
+        console.log(error);
+        initPrompt();
+    }
 };
 
 const addEmployee = () => {
@@ -86,8 +120,6 @@ const addEmployee = () => {
         const roleQuery = `SELECT * from role`
         connection.query(roleQuery, async (err, results) => {
             if (err) throw err;
-            /* let roles = await connection.query('SELECT * FROM role');
-            let managers = await connection.query('SELECT * FROM employee') */
             await inquirer.prompt([
                 {
                     name: 'firstName',
@@ -123,3 +155,85 @@ const addEmployee = () => {
     }
 };
 
+const removeEmployee = () => {
+    try {
+        const roleQuery = `SELECT * from employee`
+        connection.query(roleQuery, async (err, results) => {
+            if (err) throw err;
+            await inquirer.prompt([
+                {
+                    name: 'employee',
+                    message: 'Which employee would you like to remove?',
+                    type: 'list',
+                    choices: results.map(employee =>
+                        employee.first_name
+                    ),
+                },
+            ]).then(function (response) {
+                const chosenEmp = results.find(employee => employee.first_name === response.employee)
+                connection.query('DELETE FROM employee WHERE ?', {
+                    id: chosenEmp.id,
+                })
+            })
+            initPrompt();
+        });
+    } catch (error) {
+        console.log(error);
+        initPrompt();
+    }
+};
+
+const updateRole = async () => {
+    connection.query('SELECT * FROM employee', (err, results) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'employee',
+                message: 'Which employee would you like to update their role?',
+                type: 'list',
+                choices: results.map(employee => employee.first_name)
+            },
+        ]).then(function (response) {
+            connection.query('SELECT * FROM role', (err, roleResponse) => {
+                if (err) throw err;
+                inquirer.prompt([
+                    {
+                        name: 'role',
+                        message: 'What is their new role?',
+                        input: 'list',
+                        choices: roleResponse.map(role => role.title),
+                    },
+                ]);
+                const newRole = roleResponse.find(role => role.title === response.role)
+
+                connection.query('UPDATE employee SET role_id = ? WHERE first_name = ?', [newRole.id, first_name], (err, result) => {
+                    if (err) throw err;
+                })
+            });
+        });
+    });
+}
+
+const budgetByDepartment = async () => {
+    connection.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'department',
+                message: 'Which department budget would you like to view?',
+                type: 'list',
+                choices: results.map(department => department.department_name)
+            },
+        ])
+    }).then(function (response) {
+        const deptChoice = results.find(department => department.department_name === response.department_name)
+        const deptQuery = `SELECT role.title AS 'Title', department.department_name AS 'Department', role.salary AS 'Salary'
+                            FROM role, department
+                            WHERE department_name = ?
+                            AND department.id = role.department_id`
+        connection.query(deptQuery, [deptChoice.department_name], (err, res) => {
+            if (err) throw err;
+            console.table(res);
+        })
+    });
+}
